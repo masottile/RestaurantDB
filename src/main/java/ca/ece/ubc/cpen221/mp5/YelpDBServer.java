248ebab8c5c77ff3dc958250a60c4ccb3b7f09f6
Maria Sottile <37maria.sottile@gmail.com>
2017-11-28 15:21:49 -0800

    more stuff for the server, better implementation

--- src/main/java/ca/ece/ubc/cpen221/mp5/YelpDBServer.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/YelpDBServer.java
@@ -10,86 +10,81 @@ import java.net.Socket;
 
 public class YelpDBServer {
 
-	private static ServerSocket serverSocket;
-	//private static Socket clientSocket;
-	
 	private static int portNumber;
-	private static boolean isStopped = false;
-	private static YelpDB theDB;
+	private ServerSocket serverSocket;
 
-	public void main(String[] args) {
-
-		serverSocket = null;
-		
-
+	public static void main(String[] args) {
 		portNumber = Integer.parseInt(args[0]);
-		
-		// This block tries to initialize the things with the yelpDB wrapping stuffs
-		initializeDB();
-		
-		// This block opens up the server socket to listen at the given port number
-		openServer();
 
-		// while the server is still running, it will listen for clients
-		while (!isStopped) {
-			Socket clientSocket = null;
-			// this block tries to accept a client request
-			acceptClient(clientSocket);
-			// TODO is this right?
-
-			// if the client is successful, then we create a new thread to deal with
-			// whatever the fuck they want
-			new Thread(new dBRunnable(clientSocket)).start();
-
-		}
-		
-		// our server is no longer running
-		System.out.println("Server stopped");
-
-	}
-	
-	private static void initializeDB() {
 		try {
-			theDB = new YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json");
-		} catch (FileNotFoundException e) {
-			System.out.println("fucked up the file names or some shit");
-		}
-	}
-	
-	private static void openServer() {
-		try {
-			serverSocket = new ServerSocket(portNumber);
+			YelpDBServer dBServer = new YelpDBServer(portNumber);
+			dBServer.listen();
 		} catch (IOException e) {
-			System.out.println("you fucked up with creating the server");
+			System.out.println("rip you");
 		}
 	}
 	
-	private static void acceptClient(Socket clientSocket) {
-		try {
-			clientSocket = serverSocket.accept();
-		} catch (IOException e) {
-			System.out.println("you fucked up accepting the client");
-		}
+	private YelpDBServer(int portNumber) throws IOException{
+		this.portNumber = portNumber;
+		serverSocket = new ServerSocket(portNumber);
 	}
-
-	private class dBRunnable implements Runnable {
-
+	
+	private void listen() {
 		
-		private Socket client;
-		public dBRunnable(Socket clientSocket) {
-			client = clientSocket;
-			System.out.println("YAY YOU");
-		}
-
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-			System.out.println("YAY YOU");
-			
-			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-	        PrintWriter out = new PrintWriter(client.getOutputStream());
-			// here is where we need to handle requests from the user.
-		}
-
 	}
+	/*
+	 * private static ServerSocket serverSocket; private Socket clientSocket;
+	 * 
+	 * private static int portNumber; private static boolean isStopped = false;
+	 * private static YelpDB theDB;
+	 * 
+	 * public static void main(String[] args) {
+	 * 
+	 * serverSocket = null;
+	 * 
+	 * portNumber = Integer.parseInt(args[0]);
+	 * 
+	 * // This block tries to initialize the things with the yelpDB wrapping stuffs
+	 * initializeDB();
+	 * 
+	 * // This block opens up the server socket to listen at the given port number
+	 * openServer();
+	 * 
+	 * // while the server is still running, it will listen for clients while
+	 * (!isStopped) { // this block tries to accept a client request
+	 * acceptClient(clientSocket); // TODO is this right?
+	 * 
+	 * // if the client is successful, then we create a new thread to deal with //
+	 * whatever the fuck they want new Thread(new dBRunnable(clientSocket)).start();
+	 * 
+	 * }
+	 * 
+	 * // our server is no longer running System.out.println("Server stopped");
+	 * 
+	 * }
+	 * 
+	 * private static void initializeDB() { try { theDB = new
+	 * YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json"); }
+	 * catch (FileNotFoundException e) {
+	 * System.out.println("fucked up the file names or some shit"); } }
+	 * 
+	 * private static void openServer() { try { serverSocket = new
+	 * ServerSocket(portNumber); } catch (IOException e) {
+	 * System.out.println("you fucked up with creating the server"); } }
+	 * 
+	 * private void acceptClient(Socket clientSocket) { try { clientSocket =
+	 * serverSocket.accept(); } catch (IOException e) {
+	 * System.out.println("you fucked up accepting the client"); } }
+	 * 
+	 * 
+	 * @Override public void run() { // TODO Auto-generated method stub
+	 * System.out.println("YAY YOU");
+	 * 
+	 * BufferedReader in = new BufferedReader(new
+	 * InputStreamReader(clientSocket.getInputStream())); PrintWriter out = new
+	 * PrintWriter(clientSocket.getOutputStream()); // here is where we need to
+	 * handle requests from the user.
+	 * 
+	 * }
+	 */
 }
