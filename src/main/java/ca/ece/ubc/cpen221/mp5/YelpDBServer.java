702f2ae1dd0298fc48f238c1098f473960d5f77c
gejessicama <gejessicama@gmail.com>
2017-11-29 19:09:21 -0800

    done kMeans testing and a decent start to server creation?

--- src/main/java/ca/ece/ubc/cpen221/mp5/YelpDBServer.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/YelpDBServer.java
@@ -1,90 +1,100 @@
 package ca.ece.ubc.cpen221.mp5;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class YelpDBServer {
+	/** Default port number where the server listens for connections. */
+	public static final int YELP_PORT = 4949;
 
-	private static int portNumber;
 	private ServerSocket serverSocket;
 
-	public static void main(String[] args) {
-		portNumber = Integer.parseInt(args[0]);
+	/**
+	 * Makes server that listens for connections on some input port\
+	 * 
+	 * @param port
+	 *            Rep invariant: 0 <= port <= 65535
+	 */
+	public YelpDBServer(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+	}
 
-		try {
-			YelpDBServer dBServer = new YelpDBServer(portNumber);
-			dBServer.listen();
-		} catch (IOException e) {
-			System.out.println("rip you");
+	/**
+	 * Run the server, listening for connections and handling them.
+	 * 
+	 * @throws IOException
+	 *             if the main server socket is broken
+	 */
+	public void serve() throws IOException {
+		while (true) {
+			// waits for a client to connect
+			final Socket socket = serverSocket.accept();
+			// create a new thread to handle that client
+			Thread handler = new Thread(new Runnable() {
+				public void run() {
+					try {
+						try {
+							handle(socket);
+						} finally {
+							socket.close();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			// starts a thread
+			handler.start();
 		}
 	}
-	
-	private YelpDBServer(int portNumber) throws IOException{
-		this.portNumber = portNumber;
-		serverSocket = new ServerSocket(portNumber);
-	}
-	
-	private void listen() {
-		//this listens for a new client 
-	}
-	/*
-	 * private static ServerSocket serverSocket; private Socket clientSocket;
+
+	/**
+	 * Handle one client connection. Returns when client disconnects.
 	 * 
-	 * private static int portNumber; private static boolean isStopped = false;
-	 * private static YelpDB theDB;
-	 * 
-	 * public static void main(String[] args) {
-	 * 
-	 * serverSocket = null;
-	 * 
-	 * portNumber = Integer.parseInt(args[0]);
-	 * 
-	 * // This block tries to initialize the things with the yelpDB wrapping stuffs
-	 * initializeDB();
-	 * 
-	 * // This block opens up the server socket to listen at the given port number
-	 * openServer();
-	 * 
-	 * // while the server is still running, it will listen for clients while
-	 * (!isStopped) { // this block tries to accept a client request
-	 * acceptClient(clientSocket); // TODO is this right?
-	 * 
-	 * // if the client is successful, then we create a new thread to deal with //
-	 * whatever the fuck they want new Thread(new dBRunnable(clientSocket)).start();
-	 * 
-	 * }
-	 * 
-	 * // our server is no longer running System.out.println("Server stopped");
-	 * 
-	 * }
-	 * 
-	 * private static void initializeDB() { try { theDB = new
-	 * YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json"); }
-	 * catch (FileNotFoundException e) {
-	 * System.out.println("fucked up the file names or some shit"); } }
-	 * 
-	 * private static void openServer() { try { serverSocket = new
-	 * ServerSocket(portNumber); } catch (IOException e) {
-	 * System.out.println("you fucked up with creating the server"); } }
-	 * 
-	 * private void acceptClient(Socket clientSocket) { try { clientSocket =
-	 * serverSocket.accept(); } catch (IOException e) {
-	 * System.out.println("you fucked up accepting the client"); } }
-	 * 
-	 * 
-	 * @Override public void run() { // TODO Auto-generated method stub
-	 * System.out.println("YAY YOU");
-	 * 
-	 * BufferedReader in = new BufferedReader(new
-	 * InputStreamReader(clientSocket.getInputStream())); PrintWriter out = new
-	 * PrintWriter(clientSocket.getOutputStream()); // here is where we need to
-	 * handle requests from the user.
-	 * 
-	 * }
+	 * @param socket
+	 *            socket where client is connected
+	 * @throws IOException
+	 *             if connection encounters an error
 	 */
+	private void handle(Socket socket) throws IOException {
+		System.err.println("Connected to server!");
+
+		// converts socket byte stream to character stream
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+
+		try {
+			System.err.println("enter an integer and get that integer squared");
+			for (String line = in.readLine(); line != null; line = in.readLine()) {
+				System.err.println("request: " + line);
+				try {
+					int x = Integer.valueOf(line);
+					int y = x * x;
+					System.err.println("reply: " + y);
+					out.println(y);
+
+				} catch (NumberFormatException e) {
+					System.err.println("please enter a valid integer");
+					out.print("error\n");
+				}
+			}
+		} finally {
+			out.close();
+			in.close();
+		}
+	}
+
+	/**
+	 * Start WelpDB on the default port = 4949
+	 */
+	public static void main(String[] args) {
+		try {
+			YelpDBServer server = new YelpDBServer(YELP_PORT);
+			server.serve();
+		} catch (IOException e) {
+			System.out.println("main function not working");
+			e.printStackTrace();
+		}
+	}
 }
